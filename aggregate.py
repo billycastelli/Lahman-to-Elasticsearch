5aea61b756e40dbf6d8e59a238ad0645872e39eb
Billy Castelli <billycastelli@gmail.com>
2021-02-20 21:22:02 -0800

    Reformat field names

--- aggregate.py
+++ aggregate.py
@@ -1,4 +1,6 @@
 from utils import cleanAverage, careerBattingBase, cleanStat
+from SQLiteWrapper import SQLiteWrapper
+
 
 # Functions to aggregate and join player information into dictionary format
 
@@ -7,21 +9,21 @@ def getPlayerDocument(p, cursor):
     """
     Return a dict object with a single player's career stats
     """
-    player = dict(zip(p.keys(), p))
-    print(player['playerID'])
+    player = dict(zip((x.lower() for x in p.keys()), p))
+    print(player['playerid'])
     player['career_batting'] = careerBattingBase()
-    player['name'] = f"{player['nameFirst']} {player['nameLast']}"
+    player['name'] = f"{player['namefirst']} {player['namelast']}"
     player['batting'] = []
     player['fielding'] = []
     if 'debut' in player and player['debut'] == '':
         player['debut'] = None
-    if 'finalGame' in player and player['finalGame'] == '':
-        player['finalGame'] = None
+    if 'finalgame' in player and player['finalgame'] == '':
+        player['finalgame'] = None
     cursor.execute(
-        f"SELECT * FROM batting where playerID = '{player['playerID']}'")
+        f"SELECT * FROM batting where playerID = '{player['playerid']}'")
     battingLines = cursor.fetchall()
     for line in battingLines:
-        line = dict(zip(line.keys(), line))
+        line = dict(zip((x.lower() for x in line.keys()), line))
         line['avg'] = cleanAverage(line['hits'], line['ab'])
         for stat in line:
             if stat in player['career_batting']:
@@ -32,12 +34,16 @@ def getPlayerDocument(p, cursor):
     return player
 
 
-def getAllPlayers(playerList, cursor):
+def getAllPlayers(dbName):
     """
-    Return all player documents in list format 
+    Return all player documents in list format
     """
+    # Grab all player information
     allPlayers = []
-    for p in playerList:
-        playerDocument = getPlayerDocument(p, cursor)
-        allPlayers.append(playerDocument)
+    with SQLiteWrapper(dbName) as cursor:
+        cursor.execute("SELECT * FROM people;")
+        players = cursor.fetchall()
+        for p in players:
+            playerDocument = getPlayerDocument(p, cursor)
+            allPlayers.append(playerDocument)
     return allPlayers
